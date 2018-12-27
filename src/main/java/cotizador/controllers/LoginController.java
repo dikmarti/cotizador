01dacc936962c3ee1224a5aeec85a89af95fabf3
Diana <Diana@192.168.15.4>
2018-12-27 00:10:31 -0300

    Eliminar al usuario de la session en logout

--- src/main/java/cotizador/controllers/LoginController.java
+++ src/main/java/cotizador/controllers/LoginController.java
@@ -26,6 +26,8 @@ import cotizador.service.LoginService;
 @Produces(MediaType.TEXT_HTML)
 public class LoginController {
 	
+	private static final String USUARIO_SESSION = "usuario";
+
 	Logger log = Logger.getLogger(LoginController.class); 
 
 	@Inject
@@ -49,7 +51,7 @@ public class LoginController {
 			
 			if(usuario != null) {				
 				HttpSession httpSession = httpRequest.getSession();
-				httpSession.setAttribute("usuario", usuario.getLogin());
+				httpSession.setAttribute(USUARIO_SESSION, usuario.getLogin());
 			}
 			
 									
@@ -68,6 +70,46 @@ public class LoginController {
 		}       
       System.out.println("usuario " + usuario.getLogin());
         return usuario;
+    }
+	
+	@POST
+    @Path("/out")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+    public Usuario logout(String jsonForm, @Context HttpServletRequest httpRequest) {
+		
+		ObjectMapper mapper=new ObjectMapper();
+		try {
+			String usuario = mapper.readValue(jsonForm, String.class);
+			
+			 System.out.println("/Logout json " + usuario);
+		    	
+		        if(usuario != null && !usuario.isEmpty()) {
+		        	
+		        	   System.out.println("/usuario no vacio " + usuario);
+		        	HttpSession httpSession = httpRequest.getSession();
+					String usuarioLogueado = (String) httpSession.getAttribute(USUARIO_SESSION);
+					
+					System.out.println("/usuario loggedd " + usuarioLogueado);
+					
+					if(usuario.equals(usuarioLogueado)) {
+						
+						System.out.println("/usuario session equals json " + usuario);
+						httpSession.removeAttribute(USUARIO_SESSION);
+					}
+		        }
+		} catch (JsonParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}       
+              
+        return new Usuario();
     }
   
 }
